f0707fee4954a7629b8cf0c954af2f0a190f597c
pulkit-gpt <hiiampulkitg@gmail.com>
2024-02-18 00:23:35 +0530

    Refactor index.js to use Express and add a basic route

--- source/index.js
+++ source/index.js
@@ -1,4 +1,17 @@
-function meow() {
-	console.log("meow");
-}
-meow();
+import express from "express";
+import "express-async-errors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.status(200).send("Hello from the miniproj API\n- By Pupta & DVD.");
+});
+
+app.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
+});
