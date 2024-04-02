3ab48d7995489dfe91fcea921d5c62bd61af0ac7
pulkit-gpt <hiiampulkitg@gmail.com>
2024-04-02 22:56:35 +0530

    added multiple ip adresses

--- source/index.js
+++ source/index.js
@@ -7,20 +7,25 @@ import routes from "./routes/index.js";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
-export var esp_url = "";
+export var esp_url = {};
 app.use(cors());
 app.use(express.json());
 app.use("/", routes);
 app.get("/", (req, res) => {
 	res.status(200).send("Hello from the miniproj API\n- By Pupta & DVD.");
 });
+
 app.post("/ip", (req, res) => {
 	let data = req.body;
 	console.log(data.ip);
-	esp_url = `http://${data.ip}:5000`;
-	console.log(esp_url);
-	res.json({ message: `IP: ${esp_url}` });
+	let url = `http://${data.ip}:5000`;
+	console.log(url);
+	esp_url[data.room] = url;
+	res.json({
+		message: `ip udated for room ${data.room} with ${esp_url[data.room]}`
+	});
 });
+
 app.listen(port, () => {
 	console.log(`Server is running on http://localhost:${port}`);
 });
